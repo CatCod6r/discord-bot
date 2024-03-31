dfe94ad752256beb594c4667883d1b92b5b0783f
CatCod6r <dadawdawdawdawdawdaw@gmail.com>
2024-03-31 13:10:26 +0300

    idk

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,17 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] arguments) throws Exception {
+        Dotenv dotenv = Dotenv.configure()
+                .filename("env")
+                .load();
+       JDA api = JDABuilder.createDefault(dotenv.get("BOT_TOKEN")).build();
 
     }
 }
